8a0bcddd720782c77ba283d41fd85096f14e10bf
YoniIfrah <yonifrah996@gmail.com>
2025-06-15 19:19:15 +0300

    fixed message format

--- helpers.py
+++ helpers.py
@@ -1,4 +1,15 @@
+import re
 from typing import Dict, List
+
+
+def format_value(value: str) -> str:
+    # Match number and optional suffix like (3.73%)
+    match = re.match(r"([-+]?\d*\.?\d+)(.*)", value)
+    if match:
+        number_part = float(match.group(1))
+        suffix = match.group(2)
+        return f"{number_part:,.2f}{suffix}"
+    return value  # fallback if it doesn't match
 
 
 def format_number(num) -> str:
@@ -12,7 +23,7 @@ def precentage_to_close(data: Dict[str, float]) -> Dict[str, str]:
         if key == "Close" or value is None:
             continue
         percentage = ((value / close) - 1) * 100
-        percentage = round(abs(percentage), 2)
+        percentage = round(percentage, 2)
         data[key] = f"{value} ({percentage}%)"
     # Optionally format Close as well:
     data["Close"] = f"{close}"
--- message.py
+++ message.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import pytz
 
-from helpers import format_number, precentage_to_close
+from helpers import format_number, format_value, precentage_to_close
 from sma_analyzer import get_total_stocks_sma
 from Stock.data import StockData
 
@@ -16,7 +16,7 @@ def message_generator(analyzer: StockData):
     support, resistance = key_levels[0], key_levels[1]
     gaps_list = analyzer.get_open_gaps()
     gaps_list[0].price_level
-    gaps_list = [round(gap.price_level,2) for gap in gaps_list]
+    gaps_list = [round(gap.gap_from,2) for gap in gaps_list]
     # add gaps to data
     data = {
         "Resistance": resistance,
@@ -26,7 +26,7 @@ def message_generator(analyzer: StockData):
         "SMA94": sma_slow
     }
     for index, gap in enumerate(gaps_list):
-        data[f"Gap {index+1}"] = gap
+        data[f"Gap {len(gaps_list)- index}"] = gap
 
     # Inject percentages
     data = precentage_to_close(data)
@@ -38,7 +38,7 @@ def message_generator(analyzer: StockData):
     current_time = datetime.now(ny_timezone).strftime("%d-%m-%Y %H:%M")       
 
     message = f"{current_time}\n" + "\n".join(
-        f"{key}: {value}" for key, value in sorted_items
+        f"{key}: {format_value(value)}" for key, value in sorted_items
     )
     message += f"\n{get_total_stocks_sma()}"
     # print(message)
